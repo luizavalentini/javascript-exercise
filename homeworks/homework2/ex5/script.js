3c572c9520319c9d6dd40b8c51a94ebc8c10163c
Luiza Valentini <luizadaravalentini@gmail.com>
2022-11-01 12:39:38 -0300

    fixing exercise 5

--- homeworks/homework2/ex5/script.js
+++ homeworks/homework2/ex5/script.js
@@ -3,16 +3,16 @@ var numero2 = document.querySelector('#numero2')
 var resultado = document.querySelector('span')
 
 function somar(){
-    resultado.innerHTML = persentInt(n1.value) + perseInt(n2.value);
+    resultado.innerHTML = parseInt(numero1.value) + parseInt(numero2.value);
 }
 function subtrair(){
-    resultado.innerHTML = persentInt(n1.value) - perseInt(n2.value);
+    resultado.innerHTML = parseInt(numero1.value) - parseInt(numero2.value);
 }
 
 function multiplicar(){
-    resultado.innerHTML = persentInt(n1.value) * perseInt(n2.value);
+    resultado.innerHTML = parseInt(numero1.value) * parseInt(numero2.value);
 }
 
 function dividir(){
-    resultado.innerHTML = persentInt(n1.value) / perseInt(n2.value);
+    resultado.innerHTML = parseInt(numero1.value) / parseInt(numero2.value);
 }
